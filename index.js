1598f81a1666e4d7adec712ed994c5025f4ad829
Bahar <floresjune132@gmail.com>
2020-09-23 20:20:12 +0300

    update index.html

---
+++ index.js
@@ -0,0 +1,14 @@
+function calculate() {
+
+    const n = document.getElementById("user_num").value;
+
+    function fibonacci(n) {
+        if (n === 0) return 0
+        else if (n === 1) return 1
+        else return fibonacci(n - 1) + fibonacci(n - 2)
+    }
+
+    console.log(fibonacci(n));
+    let result = fibonacci(n);
+    document.getElementById("result").innerHTML = "The " + n + " th fibonacci number is " + result;
+}
