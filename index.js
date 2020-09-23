5b59ad17abedfcc691b1c9c107dbeb752847ec6c
Bahar <65675010+E2802-BAHAR@users.noreply.github.com>
2020-09-23 23:22:56 +0300

    fixed codes

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function calculate() {
     const n = document.getElementById("user_num").value;
 
     function fibonacci(n) {
-        if (n === 0) return 0
-        else if (n === 1) return 1
+        if (n == 0) return 0
+        else if (n == 1) return 1
         else return fibonacci(n - 1) + fibonacci(n - 2)
     }
 
